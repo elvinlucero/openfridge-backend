ac5c4ddfada68c6dd541f9d72912cdc6a8f55544
Elvin Lucero <elvinlucero@gmail.com>
2011-10-07 11:18:44 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Openfridge::Application.config.secret_token = '307b5bda68b5628d1580ef34f2a84d300c31cf94e711674e0fa12f5237c7195c23963ee096a7e62fb2371ffe4c71384bbabb38e8af66ae16e9d14d0868319761'
